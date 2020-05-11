53bf812634e36fef234825c3e577760d0e1c461f
dDab-panda <aayush.bcode@gmail.com>
2020-05-11 15:50:58 +0530

    initial commit

---
+++ src/main/java/com/simulator/webapi/WebApiSimulatorApplication.java
@@ -0,0 +1,13 @@
+package com.simulator.webapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebApiSimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebApiSimulatorApplication.class, args);
+	}
+
+}
