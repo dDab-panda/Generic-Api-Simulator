2c77fafc07fb34651cbb74d93f46fa4833d77d3c
dorrero <saivamshi.samala@gmail.com>
2020-05-13 02:23:47 +0530

    Created a very basic controller for /configs

---
+++ src/main/java/com/simulator/webapi/config/ConfigController.java
@@ -0,0 +1,32 @@
+package com.simulator.webapi.config;
+
+import java.io.File;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.simulator.pojo.*;
+
+@RestController
+public class ConfigController {
+	
+	@RequestMapping("/configs")
+	public Response getResp() {
+		
+		// load the configuration JSON file into the an object
+		// of the (already) defiened POJO class
+		ObjectMapper mapper = new ObjectMapper();
+		Config cnf = null;
+
+		try {
+			cnf = mapper.readValue(new File("src/main/resources/config.json"), Config.class);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		// for now, our implementation gives a very simple response
+		// which is same as the one in first endpoint in first application
+	    return cnf.getApplications().getEndpoints().get(0).getResponse();
+	}
+}
